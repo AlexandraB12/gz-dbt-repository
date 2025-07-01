c34f2867913c156442e4412ccc6befab2254c1f5
alexandraboudia@hotmail.fr <alexandraboudia@hotmail.fr>
2025-07-01 12:53:30 +0000

    calculating margin per order version 2

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -2,7 +2,8 @@ SELECT orders_id,
     date_date,
     ROUND(SUM(revenue),2) AS total_revenue, 
     SUM(quantity) AS total_quantity, 
-    SUM(purchase_cost) AS total_purchase_cost
+    SUM(purchase_cost) AS total_purchase_cost,
+    SUM(margin) AS total_margin
 FROM {{ ref('int_sales_margin') }} AS sales_margin
 GROUP BY orders_id, date_date
 ORDER BY orders_id, date_date 
