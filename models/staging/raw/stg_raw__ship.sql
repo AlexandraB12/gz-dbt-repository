2671119639d23d8dcffeecdb9ee59e92ece2401f
alexandraboudia@hotmail.fr <alexandraboudia@hotmail.fr>
2025-07-01 10:24:26 +0000

    changing ship

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,31 +1,26 @@
 WITH source AS (
-
-    SELECT * FROM {{ source('raw', 'ship') }}
-
+  SELECT * FROM `zinc-fusion-462408-v9.gz_raw_data.raw_gz_ship` 
 ),
 
 renamed AS (
-
-    SELECT
-        orders_id,
-        shipping_fee,
-        shipping_fee_1,
-        logcost,
-        ship_cost
-    FROM source
-
+  SELECT
+    orders_id,
+    shipping_fee,
+    shipping_fee_1,
+    logCost,
+    ship_cost
+  FROM source
 ),
 
 filtered AS (
-
-    SELECT
-        orders_id,
-        shipping_fee,
-        logcost,
-        CAST(ship_cost AS FLOAT64) AS ship_cost
-    FROM renamed
-    WHERE shipping_fee <> shipping_fee_1
-
+  SELECT
+    orders_id,
+    shipping_fee,
+    logCost,
+    CAST(ship_cost AS FLOAT64) AS ship_cost
+  FROM renamed
+  WHERE shipping_fee <> shipping_fee_1
 )
 
 SELECT * FROM filtered;
+
